cb12438ffd8a190b1430c1e14483de88990f347a
lakshmanan1811 <velakshman@gmail.com>
2023-11-23 16:20:40 +0530

    pages modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Navbar } from './Components/Navbar/Navbar'
+import{BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+import Login from './Pages/Login'
+
 export const App = () => {
   return (
     <div>
-    <Navbar/>
+      <BrowserRouter>
+        <Navbar/>
+          <Routes>
+            <Route path = '/' element = {<HomePage/>}/>
+            <Route path = '/login' element = {<Login />} />
+          </Routes>
+        </BrowserRouter>
+        
     </div>
   )
 }
