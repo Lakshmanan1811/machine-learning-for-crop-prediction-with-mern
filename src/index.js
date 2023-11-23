122fd070cd6018943d99ff9970275e5956a79999
lakshmanan1811 <velakshman@gmail.com>
2023-11-23 14:00:46 +0530

    navbar created

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
+import navbar from './components/navbar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <navbar />
   </React.StrictMode>
 );
 
