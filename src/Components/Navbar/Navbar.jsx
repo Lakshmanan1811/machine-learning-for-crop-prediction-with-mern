e170718a7efd61bf2985f7f7f8d931b5514574ec
lakshmanan1811 <velakshman@gmail.com>
2023-11-23 14:48:37 +0530

    file structure is done

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import './Navbar.css';
+export const Navbar = () => {
+    const [menu, setMenu] = useState("home")
+  return (
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <p>PlantApp</p>
+        </div>
+        <ul className='nav-menu'>
+            <li onClick={() => {setMenu("home")}}>Home{menu === "home" ?  <hr/> : <></>}</li>
+            <li onClick={() => {setMenu("contact")}}>Contact{menu === "contact" ?  <hr/> : <></>}</li>
+            <li onClick={() => {setMenu("crops")}}>Crops{menu === "crops" ?  <hr/> : <></>}</li>
+            <li onClick={() => {setMenu("soil")}}>Soil{menu === "soil" ?  <hr/> : <></>}</li>
+        </ul>
+        <div className='nav-login'>
+            <button>Login</button>
+        </div>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { App } from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <h1>Crop and Soil Recommendation System</h1>
+  <App/>
   </React.StrictMode>
 );
 
